8d00921f7e9df7368b0622243338937a1cc949f1
zikram013 <32304189+zikram013@users.noreply.github.com>
2021-09-07 19:00:43 +0200

    fin de la parte 28 curso udemy

--- Parte1PreProcesadoDeDatos/DataProcessingTemplate.py
+++ Parte1PreProcesadoDeDatos/DataProcessingTemplate.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 # Importar el dataSet
 # Para mostrar el dataFrame del dataset debemos debuguear la variable
@@ -15,5 +18,11 @@ y = dataset.iloc[:, 3].values
 # Reemplazamiento de los valores desconocidos (0=columna 1=fila)
 imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
 imputer.fit(x[:, 1:3])
-x[:,1:3] = imputer.transform(x[:,1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+labelenconderX = preprocessing.LabelEncoder()
+x[:, 0] = labelenconderX.fit_transform(x[:, 0])
+ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x), dtype=np.float)
+labelenconderY = preprocessing.LabelEncoder()
+y=labelenconderY.fit_transform(y)
 print()
