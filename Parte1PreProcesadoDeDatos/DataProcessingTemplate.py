c960b473111cdaa2af7e9a4e4bf65bab900e7b54
zikram013 <32304189+zikram013@users.noreply.github.com>
2021-09-08 13:48:15 +0200

    Update DataProcessingTemplate.py

--- Parte1PreProcesadoDeDatos/DataProcessingTemplate.py
+++ Parte1PreProcesadoDeDatos/DataProcessingTemplate.py
@@ -25,4 +25,20 @@ ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(categories='auto'), [0
 x = np.array(ct.fit_transform(x), dtype=np.float)
 labelenconderY = preprocessing.LabelEncoder()
 y=labelenconderY.fit_transform(y)
+
+
+"""Realizado en gis, comprobar imports en casa"""
+
+# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+from skalearn.model_selection import train_test_split
+xTrain,xTest,yTrain,yTest = train_test_split(x,y,test_size = 0.2, random_state = 0) # EL 20% (tope 30%) de los datos se dedicaran a testing y el reto como conjunto de entrenamiento
+
+# Escalado de variables
+from sklearn.preprocessing import StandardScaler
+scX=StandardScaler()
+xTrain = scX.fit_transform(xTrain)
+xTest = scX.transform(xTest)
+"""¿Se deben escalar las variables dummies? Si, hay que escalarlas por coherencia para que queden  en el mismo rango
+No, no hay que escalarlas porque al hacerlo perdemos la pertenencia a la clase dummy al hacerlas y perdemos ese significado"""
+
 print()
