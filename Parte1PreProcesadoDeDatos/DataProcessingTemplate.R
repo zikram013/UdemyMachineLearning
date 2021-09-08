45fa66de3339127224f5b6e4c4058a775f5145be
zikram013 <32304189+zikram013@users.noreply.github.com>
2021-09-08 13:48:53 +0200

    Update DataProcessingTemplate.R

--- Parte1PreProcesadoDeDatos/DataProcessingTemplate.R
+++ Parte1PreProcesadoDeDatos/DataProcessingTemplate.R
@@ -19,3 +19,16 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                            levels=c("No","Yes"),
                             labels = c(0,1))
+                        
+                        
+# Dividir los datos en conjunto de entrenamiento y conjunto de test
+install.packages("caTools") # Instalar paquetes, solo hacerlo una vez y dejar comentada
+library(caTools) # Para cargar la libreria
+set.seed(123) # Definir la semilla aleatoria
+split = sample.split(dataset$Purchased,SplitRatio=0.8) # Definimos que porcentaje de los datos van a formar parte del conjunto de entrenamiento y el resto de los datos seran para testing
+training_set = subset(dataset,split == TRUE) # Cogemos el subconjunto de datos que tengan TRUE que son los de entrenamiento
+testing_set = subset(dataset,split==FALSE)  # Cogemos el subconjunto de datos que tengan FALSE que son los de testing
+
+# Escalado de valores
+training_set[,2:3] = scale(training_set[,2:3])
+testing_set[,2:3] = scale(testing_set[,2:3])
