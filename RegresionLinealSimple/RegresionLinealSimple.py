4750993defa02b318ee11681b12e20eff21aca42
zikram013 <32304189+zikram013@users.noreply.github.com>
2021-09-13 13:29:03 +0200

    Create RegresionLinealSimple.py

---
+++ RegresionLinealSimple/RegresionLinealSimple.py
@@ -0,0 +1,27 @@
+#Regresion lineal simple
+
+#Copiar Plantilla de pre procesado de datos
+
+import numpy as np
+import matplotlib.pylot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+from sklearn.model_selection import train_test_split
+xTrain,xTest,yTrain,yTest = train_test_split(x,y,test_size=1/3,random_state = 0)
+
+#Crear modelo de regresion lineal simple con el conjunto de entrenamiento
+
+from sklearn.linear_model import LinearRegression
+regression = LinearRegression() # Como es una regresion lineal simple no hace falta pasarle ningun tipo de parámetros
+regression.fit(xTrain,yTrain) #Acepta dos parametros, en primer lugar el conjunto de variables independientes o la matriz de características
+# Ambos deben tener el mismo tamaño
+
+# Predecir el conjunto de los test
+yPred = regression.predict (xTest)
+ 
+
+#
